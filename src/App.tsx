9d496ea0e713f3918a6d3ccda3c9d8a3bd9dda91
SomeYoGuy <soumyopurkayastha@gmail.com>
2023-04-11 17:54:55 +0530

    Dark Mode and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { Grid, GridItem, Show } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Grid templateAreas={{
-        md: `"nav nav" "aside main"`,
-        base: `"nav" "main"`
-      }}
+      <Grid
+        templateAreas={{
+          md: `"nav nav" "aside main"`,
+          base: `"nav" "main"`,
+        }}
       >
-        <GridItem area={"nav"} bg={"coral"}>
-          Nav
+        <GridItem area={"nav"}>
+          <NavBar />
         </GridItem>
-        <Show above='md'>
-          <GridItem area={"aside"} bg={"red"}>
+        <Show above="md">
+          <GridItem area={"aside"} bg={"yellow"}>
             Aside
           </GridItem>
         </Show>
@@ -21,7 +23,7 @@ function App() {
         </GridItem>
       </Grid>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
