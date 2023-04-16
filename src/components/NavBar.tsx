ce30b11b4144d51c7eec636d990f3c887a800140
SomeYoGuy <soumyopurkayastha@gmail.com>
2023-04-16 18:13:39 +0530

    home button made functional

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBox from "./SearchBox";
@@ -11,7 +11,9 @@ const NavBar = ({ onSearch }: Props) => {
     return (
         <>
             <HStack padding={"10px"}>
-                <Image src={logo} boxSize={"70px"} />
+                <button onClick={() => window.location.reload()}>
+                    <Image src={logo} boxSize={"80px"} overflow={"visible"} />
+                </button>
                 <SearchBox onSearch={onSearch} />
                 <ColorModeSwitch />
             </HStack>
